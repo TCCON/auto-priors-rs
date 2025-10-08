6a4c1dc207742eccd1ed1021fd4777cef58b7f23
Joshua Laughner <jllacct119@gmail.com>
2025-10-07 22:40:28 -0700

    Commented out test user util function
    This doesn't work with the standard database as it tries to insert
    into a view.

--- web-app/tests/common/mod.rs
+++ web-app/tests/common/mod.rs
@@ -1,15 +1,15 @@
 use djangohashers;
 use orm::MySqlConn;
 
-pub(crate) async fn add_test_user(conn: &mut MySqlConn) -> anyhow::Result<()> {
-    let pw_hash = djangohashers::make_password("qwerty");
-    sqlx::query!(
-        "INSERT INTO v_auth_user(password,username,email) VALUES(?,?,?)",
-        pw_hash,
-        "testuser",
-        "test@test.net"
-    )
-    .execute(conn)
-    .await?;
-    Ok(())
-}
+// pub(crate) async fn add_test_user(conn: &mut MySqlConn) -> anyhow::Result<()> {
+//     let pw_hash = djangohashers::make_password("qwerty");
+//     sqlx::query!(
+//         "INSERT INTO v_auth_user(password,username,email) VALUES(?,?,?)",
+//         pw_hash,
+//         "testuser",
+//         "test@test.net"
+//     )
+//     .execute(conn)
+//     .await?;
+//     Ok(())
+// }
